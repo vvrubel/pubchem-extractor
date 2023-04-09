099bee496057bfba06920bbb9ff5d89a8ff05947
Anna Vasileva <53346960+vvrubel@users.noreply.github.com>
2023-04-10 02:02:02 +0400

    v1: move code into `old` (#15)
    * UPD: move old code and settings to `old` dir

--- old/tests/test_molecad.py
+++ old/tests/test_molecad.py
@@ -1,9 +1,9 @@
 import pytest
 
-from molecad.downloader import chunked, generate_ids, request_data_json
-from molecad.downloader_types import Domain, NamespCmpd, Operation, OperationComplex, PropertyTags
-from molecad.utils import concat, url_encoder
-from molecad.validator import check_tags, is_complex_operation, is_simple_operation
+from old.downloader import chunked, generate_ids, request_data_json
+from old.downloader_types import Domain, NamespCmpd, Operation, OperationComplex, PropertyTags
+from old.utils import concat, url_encoder
+from old.validator import check_tags, is_complex_operation, is_simple_operation
 
 EXAMPLE1 = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/2244/property/MolecularFormula,InChIKey/JSON"
 
